ba7f23764ccbcd60f6a6b04e89384688fd086b9e
Prince <princerajo9814@gmail.com>
2025-12-18 15:43:14 +0530

    Refactor slide functions for improved clarity

--- index.js
+++ index.js
@@ -1,31 +1,24 @@
-let index = 0;
 let slides = document.getElementById("slides");
-let dots = document.querySelectorAll(".dot");
+let slideIndex = 0;
+let totalSlides = slides.children.length;
+let slideWidth = 600; 
 
-function showSlide(i){
-    index = i;
-    if(index >= dots.length) index = 0;
-    if(index < 0) index = dots.length - 1;
+function showSlide(index) {
+    if (index < 0) slideIndex = totalSlides - 1;
+    else if (index >= totalSlides) slideIndex = 0;
+    else slideIndex = index;
 
-    slides.style.transform = `translateX(${-600 * index}px)`;
-
-    dots.forEach(dot => dot.classList.remove("active"));
-    dots[index].classList.add("active");
+    slides.style.transform = `translateX(-${slideIndex * slideWidth}px)`;
 }
 
-function nextSlide(){
-    showSlide(index + 1);
+function nextSlide() {
+    showSlide(slideIndex + 1);
 }
 
-function prevSlide(){
-    showSlide(index - 1);
+function prevSlide() {
+    showSlide(slideIndex - 1);
 }
 
-function goSlide(i){
-    showSlide(i);
+function goSlide(index) {
+    showSlide(index);
 }
-
-
-setInterval(() => {
-    nextSlide();
-}, 3000);
